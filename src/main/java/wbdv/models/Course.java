d72de719a61786cb7f0401edbbf421c32fafa1c2
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-07-16 22:30:38 -0400

    Added Course data model

--- src/main/java/wbdv/models/Course.java
+++ src/main/java/wbdv/models/Course.java
@@ -1,53 +1,66 @@
-//package wbdv.models;
-//
-//import javax.persistence.*;
-//import java.sql.Date;
-//
-//@Entity
-//public class Course {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//    private String title;
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date created;
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date modified;
-//
-//    public Course() {
-//        super();
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public Date getCreated() {
-//        return created;
-//    }
-//
-//    public void setCreated(Date created) {
-//        this.created = created;
-//    }
-//
-//    public Date getModified() {
-//        return modified;
-//    }
-//
-//    public void setModified(Date modified) {
-//        this.modified = modified;
-//    }
-//}
+package wbdv.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.List;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @JsonFormat(pattern = "MMMM" + " " + "dd" + "," + "YYYY")
+    private Date created;
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "MMMM" + " " + "dd" + "," + "YYYY")
+    private Date modified;
+    @OneToMany(mappedBy = "course")
+    private List<Module> modules;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
+
+    public List<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(List<Module> modules) {
+        this.modules = modules;
+    }
+}

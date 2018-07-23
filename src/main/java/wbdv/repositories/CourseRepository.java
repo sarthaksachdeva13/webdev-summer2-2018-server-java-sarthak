1db8443e2fbb854dfd1e3ddb18babccf54b861ff
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-07-23 15:15:46 -0400

    Minor edits

--- src/main/java/wbdv/repositories/CourseRepository.java
+++ src/main/java/wbdv/repositories/CourseRepository.java
@@ -1,10 +1,8 @@
 package wbdv.repositories;
 
 import wbdv.models.Course;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
 public interface CourseRepository extends CrudRepository<Course, Integer> {
 

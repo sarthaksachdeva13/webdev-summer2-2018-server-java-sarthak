a308775aea1c63f8acd33f1402a9faaee4b23194
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-07-26 16:56:51 -0400

    Added widget repo

---
+++ src/main/java/wbdv/repositories/WidgetRepository.java
@@ -0,0 +1,8 @@
+package wbdv.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import wbdv.models.Widget;
+
+public interface WidgetRepository extends CrudRepository<Widget, Integer> {
+
+}
